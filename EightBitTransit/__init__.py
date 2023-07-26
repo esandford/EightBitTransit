b8b80708ae6954cc227ab1ed38b2cd4caf448342
Daniel Giles <daniel.k.giles@gmail.com>
2023-07-26 23:43:58 +0000

    added metadata to EightBitTransit init

--- EightBitTransit/__init__.py
+++ EightBitTransit/__init__.py
@@ -1,5 +1,9 @@
 #__all__ = ['cTransitingImage','cGridFunctions','misc','inversion']
 
+__author__ = """Daniel Giles"""
+__email__ = 'daniel.k.giles@gmail.com'
+__version__ = '1.0.0'
+
 from .cGridFunctions import *
 from .misc import *
 from .inversion import *
